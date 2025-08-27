b46c09ea16842f9c5cd2a2fe567f29175c50a3f5
alves084 <josealvesne08@gmail.com>
2025-08-27 15:55:09 -0300

    add biblioteca virtual

---
+++ assets/js/livros.js
@@ -0,0 +1,24 @@
+// Arquivo: livros.js
+const livros = [
+  {
+    titulo: "As Crônicas de Nárnia",
+    autor: "C. S. LEWIS",
+    capa: "assets/capas/cronicasdenarnia.png",
+    arquivo: "https://drive.google.com/file/d/1_2btDXvWPDpWmet66vMEJ8H5MNyJDHI8/view?usp=sharing",
+    categoria: "CONTOS"
+  },
+  {
+    titulo: "A Srta. Butterworth o o Barão Louco",
+    autor: "JULIA QUINN",
+    capa: "assets/capas/ASrtaButterworthooBarãoLouco.png",
+    arquivo: "https://drive.google.com/file/d/1sG57P1XoZi1-mZucBAMucPE_oZa8mx25/view?usp=sharing",
+    categoria: "CONTOS"
+  },
+  {
+    titulo: "A Última Batalha",
+    autor: "C. S. LEWIS",
+    capa: "assets/capas/aultimabatalha.png",
+    arquivo: "https://drive.google.com/file/d/1XyMODvOpcs827wfanT-9n695ZyjekXFO/view?usp=sharing",
+    categoria: "CONTOS"
+  },  
+];
