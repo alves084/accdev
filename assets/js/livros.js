d261bcac4227ca3776b4bfd68f1df43184ff6808
alves084 <josealvesne08@gmail.com>
2025-08-27 23:02:17 -0300

    editing css biblioteca.html (cards)

--- assets/js/livros.js
+++ assets/js/livros.js
@@ -20,5 +20,26 @@ const livros = [
     capa: "assets/capas/aultimabatalha.png",
     arquivo: "https://drive.google.com/file/d/1XyMODvOpcs827wfanT-9n695ZyjekXFO/view?usp=sharing",
     categoria: "CONTOS"
-  },  
+  },
+  {
+    titulo: "A Viagem do Peregrino da Alvorada",
+    autor: "C. S. LEWIS",
+    capa: "assets/capas/AViagemdoPeregrinodaAlvorada.png",
+    arquivo: "https://drive.google.com/file/d/1mrwZF5oLym8DLKgbMwuI9EpTeM1wgbdW/view?usp=sharing",
+    categoria: "CONTOS"
+  }, 
+  {
+    titulo: "ALICE ATRAVÉS DO ESPELHO",
+    autor: "LEWIS CARROLL",
+    capa: "assets/capas/ALICEATRAVÉSDOESPELHO.png",
+    arquivo: "https://drive.google.com/file/d/1NMRVGytUzFM9cEfs3tp75U2p06dvRD9A/view?usp=sharing",
+    categoria: "CONTOS"
+  }, 
+  {
+    titulo: "ALICE NO PAÍS DAS MARAVILHAS",
+    autor: "LEWIS CARROLL",
+    capa: "assets/capas/ALICENOPAÍSDASMARAVILHAS.png",
+    arquivo: "https://drive.google.com/file/d/1yAo6J7Bos8i-gwn2K7DoOiRreXYSM7qv/view?usp=sharing",
+    categoria: "CONTOS"
+  }, 
 ];
